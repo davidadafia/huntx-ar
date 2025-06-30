c6a52624b8235540e4c3fd610acfcecd054bf408
David Adafia <davidadafia@gmail.com>
2025-06-30 13:52:21 +0000

    Fix edit view and add full CRUD for Huntx

--- app/controllers/huntxes_controller.rb
+++ app/controllers/huntxes_controller.rb
@@ -1,9 +1,49 @@
 class HuntxesController < ApplicationController
+  before_action :set_huntx, only: [:show, :edit, :update, :destroy]
+
   def index
     @huntxes = Huntx.all
   end
 
   def show
+  end
+
+  def new
+    @huntx = Huntx.new
+  end
+
+  def create
+    @huntx = Huntx.new(huntx_params)
+    if @huntx.save
+      redirect_to @huntx, notice: 'Huntx was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @huntx.update(huntx_params)
+      redirect_to @huntx, notice: 'Huntx was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @huntx.destroy
+    redirect_to huntxes_path, notice: 'Huntx was successfully destroyed.'
+  end
+
+  private
+
+  def set_huntx
     @huntx = Huntx.find(params[:id])
+  end
+
+  def huntx_params
+    params.require(:huntx).permit(:title)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get "huntxes/show"
   root 'huntxes#index'
   get 'huntx/:id', to: 'huntxes#show', as: 'huntx'
+
+  resources :huntxes
 end
